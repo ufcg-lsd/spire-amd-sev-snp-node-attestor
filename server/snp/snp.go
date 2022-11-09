be9c41e38a878b57e0e084816d98101b2eb57eca
davi.pontes <davi.pontes@lsd.ufcg.edu.br>
2022-11-09 19:03:33 +0000

    refactor: ISSUE-9327 removendo comentarios de exemplo

--- server/snp/snp.go
+++ server/snp/snp.go
@@ -25,14 +25,7 @@ import (
 )
 
 var (
-	// This compile time assertion ensures the plugin conforms properly to the
-	// pluginsdk.NeedsLogger interface.
-	// TODO: Remove if the plugin does not need the logger.
-	_ pluginsdk.NeedsLogger = (*Plugin)(nil)
-
-	// This compile time assertion ensures the plugin conforms properly to the
-	// pluginsdk.NeedsHostServices interface.
-	// TODO: Remove if the plugin does not need host services.
+	_ pluginsdk.NeedsLogger       = (*Plugin)(nil)
 	_ pluginsdk.NeedsHostServices = (*Plugin)(nil)
 )
 
@@ -40,44 +33,24 @@ const (
 	pluginName = "sev_snp"
 )
 
-// Config defines the configuration for the plugin.
-// TODO: Add relevant configurables or remove if no configuration is required.
 type Config struct {
 	trustDomain  spiffeid.TrustDomain
 	AMDCertChain string `hcl:"amd_cert_chain"`
 }
 
-// Plugin implements the NodeAttestor plugin
 type Plugin struct {
-	// UnimplementedNodeAttestorServer is embedded to satisfy gRPC
 	nodeattestorv1.UnimplementedNodeAttestorServer
-
-	// UnimplementedConfigServer is embedded to satisfy gRPC
-	// TODO: Remove if this plugin does not require configuration
 	configv1.UnimplementedConfigServer
-
-	// Configuration should be set atomically
-	// TODO: Remove if this plugin does not require configuration
 	configMtx sync.RWMutex
 	config    *Config
-
-	// The logger received from the framework via the SetLogger method
-	// TODO: Remove if this plugin does not need the logger.
-	logger hclog.Logger
+	logger    hclog.Logger
 }
 
-// SetLogger is called by the framework when the plugin is loaded and provides
-// the plugin with a logger wired up to SPIRE's logging facilities.
-// TODO: Remove if the plugin does not need the logger.
 func (p *Plugin) SetLogger(logger hclog.Logger) {
 	p.logger = logger
 }
 
-// BrokerHostServices is called by the framework when the plugin is loaded to
-// give the plugin a chance to obtain clients to SPIRE host services.
-// TODO: Remove if the plugin does not need host services.
 func (p *Plugin) BrokerHostServices(broker pluginsdk.ServiceBroker) error {
-	// TODO: Use the broker to obtain host service clients
 	return nil
 }
 
@@ -88,7 +61,6 @@ func generateNonce(length uint8) []byte {
 	return nonce
 }
 
-// Attest implements the NodeAttestor Attest RPC
 func (p *Plugin) Attest(stream nodeattestorv1.NodeAttestor_AttestServer) error {
 	config, err := p.getConfig()
 	if err != nil {
@@ -193,10 +165,6 @@ func buildSelectorValues(report snp.AttestationReport, vcek []byte) []string {
 	return selectorValues
 }
 
-// Configure configures the plugin. This is invoked by SPIRE when the plugin is
-// first loaded. In the future, tt may be invoked to reconfigure the plugin.
-// As such, it should replace the previous configuration atomically.
-// TODO: Remove if no configuration is required
 func (p *Plugin) Configure(ctx context.Context, req *configv1.ConfigureRequest) (*configv1.ConfigureResponse, error) {
 	trustDomain, err := parseCoreConfig(req.CoreConfiguration)
 	if err != nil {
@@ -231,16 +199,12 @@ func parseCoreConfig(c *configv1.CoreConfiguration) (spiffeid.TrustDomain, error
 	return trustDomain, nil
 }
 
-// setConfig replaces the configuration atomically under a write lock.
-// TODO: Remove if no configuration is required
 func (p *Plugin) setConfig(config *Config) {
 	p.configMtx.Lock()
 	p.config = config
 	p.configMtx.Unlock()
 }
 
-// getConfig gets the configuration under a read lock.
-// TODO: Remove if no configuration is required
 func (p *Plugin) getConfig() (*Config, error) {
 	p.configMtx.RLock()
 	defer p.configMtx.RUnlock()
